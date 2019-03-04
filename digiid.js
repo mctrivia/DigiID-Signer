53ef031b0c27e72ce9abb56e71c1a3e0352ddec2
Matthew Cornelisse <squarerootofnegativeone@gmail.com>
2019-03-03 20:25:42 -0600

    Fixed value being signed

--- digiid.js
+++ digiid.js
@@ -83,8 +83,8 @@
 		"getSignature":function(base64) {	//if base64 true then will return base 64 encoded.  otherwise returns Uint8Array
 			var me=this;
 			me._safe();
-			var hash=bitcoinjs.bitcoin.crypto.hash256(me.network.messagePrefix+magicLength(me.url)+me.url);
-			var signature=me.keys.sign(hash).toCompact();
+			var hash=bitcoinjs.bitcoin.crypto.hash256(me.network.messagePrefix+magicLength(me.uri)+me.uri);
+			var signature=me.keys.sign(hash).toCompact(1,true);
 			return base64?btoa(String.fromCharCode.apply(null, signature)):signature;
 		}
 		
